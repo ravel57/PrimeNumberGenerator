901852cf9da236159f8ca0d7050a62ffafb5dd8b
petr <petrlomakin@MacBook-Pro-Petr.local>
2020-03-05 21:12:19 +0300

    first stable working version of the program

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+#define ARRSIZE 0xffffff
+#define ROUNDS 3
+
+unsigned long long  arrayOfPrimes[ARRSIZE];
+unsigned long long  checkingNumber = 1;
+unsigned long long  countOfPrimes = 1;
+unsigned long       sq;
+unsigned long       localCounter;
+bool flagPrime = true;
+
+int timeArr[ROUNDS];
+clock_t start, stop, deltaTime;
+
+int main() {
+    arrayOfPrimes[0] = 2;
+    cout << "start\n" << "count:\t" << ARRSIZE << "\n--------------\n";
+
+    for (int round = 0; round < ROUNDS; round++) {
+        start = clock();
+
+        while (countOfPrimes < ARRSIZE) {
+            checkingNumber += 2;
+
+            sq = sqrt(checkingNumber), localCounter=0;
+            while (arrayOfPrimes[localCounter] < sq)
+                localCounter++;
+
+            for (int i = 1; i <= localCounter; i++) {
+                if (checkingNumber % arrayOfPrimes[i] == 0) {
+                    flagPrime = false;
+                    break;
+                }
+            }
+            if (flagPrime) {
+                arrayOfPrimes[countOfPrimes] = checkingNumber;
+                countOfPrimes++;
+            }
+            flagPrime = true;
+        }
+
+        stop = clock();
+        deltaTime = (stop - start) / 1000;
+        timeArr[round] = deltaTime;
+        cout << round << ":\t\t" << deltaTime << " ms\n" ;//<< countOfPrimes << endl << checkingNumber << "\n\n";
+
+        checkingNumber = 1; countOfPrimes = 1;
+        for (int i = 1; i < ARRSIZE; i++) {
+            arrayOfPrimes[i] = 0;
+        }
+    }
+
+    deltaTime = 0;
+    for (int i = 0; i < ROUNDS; i++) {
+        deltaTime += timeArr[i];
+    }
+    cout << "==============\n" << "avg:\t" << deltaTime / ROUNDS << " ms\n";
+
+//    cin.get();
+    return 0;
+}
+
